afdc5ff6a338f36e8d1b9c08e3f9b3272d75eaaf
Areesha kz <157448075+Aishakz@users.noreply.github.com>
2024-06-13 05:27:41 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //Treat all JS code as newer version
+
+// JavaScript ka (document)enjine jo hai wo browser(console) m chupa huwa hai 
+
+// alert(3+3) // We are using node js, not Browser
+
+console.log(3
+    +
+      3) // Code readability should be high
+
+console.log("Areesha")
+
+let name = "Areesha" // String DataTypes
+let age = 18 //Number
+let isLoggedIn = false // Boolean DataTypes
+
+    //DataTypes
+// Number => 2 to power 53
+// bigint 
+// string => using double code ""
+// boolean => true/false
+// null => Standlone value (empty)
+// undefined 
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //Object
