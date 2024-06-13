b2da645ac7e8fe570b189218dd8bebf108a3400c
Areesha kz <157448075+Aishakz@users.noreply.github.com>
2024-06-13 06:03:25 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//Notes
+// "33"=> 33
+// "33abc"=> NAN (Not of numbers)
+// true => 1; false=>0;
+
+let isLoggedIn = "Areesha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//Notes
+// 1 => true; 0=>False
+//""=>False
+//"areesha"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
