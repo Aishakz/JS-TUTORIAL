397f2c2e56fb2c5c5aa21d0c25d369de73d3c967
Areesha kz <157448075+Aishakz@users.noreply.github.com>
2024-06-14 05:22:51 +0000

    dataType summary

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, bigint 
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false 
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+
+// console.log(id === anotherId);
+
+// const bigNumber =392473484973n
+
+//Reference (Non primitive)
+
+//Array, Object, Functions
+
+const heros = ["bilal", "wahaj", "muneeb"] // Array
+
+//Object
+let myObj = {
+    name:"Areesha",
+    age:22,
+}
+
+// Function
+const myFunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof heros)
