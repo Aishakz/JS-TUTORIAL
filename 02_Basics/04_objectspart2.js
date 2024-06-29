11cf999b06da2d301882a609b5fb8c01a79d5001
Areesha kz <157448075+Aishakz@users.noreply.github.com>
2024-06-29 06:24:38 +0000

    Obj de-structure and JSON API talk

--- 02_Basics/04_objectspart2.js
+++ 02_Basics/04_objectspart2.js
@@ -45,10 +45,36 @@ const users= [
 ]
 
 users[1].email
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+// De-Structure in JSON-Api
+const course = {
+    courseName : "JS in Hindi",
+    price:"999",
+    courseInstructor: "Areesha"
+}
+
+// course.courseInstructor
+const {courseInstructor : instructor} = course
+// console.log(courseInstructor)
+console.log(instructor)
+
+// {
+//     "name" : "Areesha",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {},
+]
+
+// json talk
